a95ba0ff1f35cd69ee5253ade01a9f96af5be46f
Woozsh <jordy.pereira@hotmail.com>
2017-12-29 11:35:47 +0100

    app

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem Ipsum Lorem Lorem Lorem',
+            'author': 'Jordy Pereira',
+            'create_date': '28-12-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem Ipsum Lorem Lorem Lorem',
+            'author': 'Meneer Petwee',
+            'create_date': '28-12-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem Ipsum Lorem Lorem Lorem',
+            'author': 'Jordy Pereira',
+            'create_date': '28-12-2018'
+        },
+    ]
+    return articles
