e878dacbee68feeafb2535c04e9dad12c1b092cb
Woozsh <jordy.pereira@hotmail.com>
2018-01-02 04:13:51 +0100

    moved config folder

---
+++ secu_school/config/default.py
@@ -0,0 +1,8 @@
+UPLOAD_FOLDER = 'secu_school/static/images/'
+# Config MySQL
+MYSQL_DB = 'secu_school'
+MYSQL_CURSORCLASS= 'DictCursor'
+MYSQL_HOST = 'perjor.mysql.pythonanywhere-services.com'
+MYSQL_USER = 'perjor'
+MYSQL_PASSWORD = '9sqjFtv9'
+SECRET_KEY = 'secret123'
