830f52e58c6b8c62630dca06a04448a36aec7b19
Woozsh <jordy.pereira@hotmail.com>
2018-01-02 04:13:25 +0100

    hey

--- config/default.py
+++
@@ -1,8 +0,0 @@
-UPLOAD_FOLDER = 'secu_school/static/images/'
-# Config MySQL
-MYSQL_DB = 'secu_school'
-MYSQL_CURSORCLASS= 'DictCursor'
-MYSQL_HOST = 'perjor.mysql.pythonanywhere-services.com'
-MYSQL_USER = 'perjor'
-MYSQL_PASSWORD = '9sqjFtv9'
-SECRET_KEY = 'secret123'
